227868635b4cde33e7c85b06f4067470903b9e44
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-01 00:45:07 +0100

    página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var Swiper: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    var homeSwiper = new Swiper(".home-swiper", {
+      spaceBetween: 30,
+      effect: "cube",
+      loop: "true",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      cubeEffect: {
+        shadow: false,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+    });
+  }
 }
