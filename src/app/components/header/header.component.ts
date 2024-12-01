7a131cb52abdd5a74d3fe9972d5d63da12d4b95e
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-01 04:44:33 +0100

    componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const header = document.getElementById('header');
+
+    if (window.scrollY >= 80) {
+      header?.classList.add('scroll-header');
+    } else {
+      header?.classList.remove('scroll-header');
+    }
+  }
 }
