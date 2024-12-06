9fc5d2429d30f8627881352af6d95a2588646f10
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-06 17:29:43 +0100

    página contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  constructor(private toastr: ToastrService) {}
+
+  newContact(form: NgForm) {
+    if (form.valid) {
+      const newContact = {
+        name: form.value.name,
+        email: form.value.email,
+        subject: form.value.subject,
+        message: form.value.message
+      };
+
+      this.toastr.success('¡Contacto enviado!');
+      form.resetForm();
+
+    } else {
+      console.log("Formulario de contacto inválido");
+    }
+  }
+
+  newReview(reviewForm: NgForm) {
+    if (reviewForm.valid) {
+      const review = {
+        rating: reviewForm.value.rating,
+        message: reviewForm.value.reviewMessage
+      };
+  
+      this.toastr.success('¡Gracias por tu valoración!');
+      reviewForm.resetForm();
+
+    } else {
+      console.log("Formulario de valoración inválido");
+    }
+  }
+  
 }
