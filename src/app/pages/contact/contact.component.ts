6609cac36dc19315070607d1cfbfdf94e05af732
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-11 14:06:20 +0100

    página contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -9,6 +9,25 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ContactComponent {
 
+  reviews = [
+    { rating: "5", 
+      name: "Alberto Garcia", 
+      message: '¡Una experiencia increíble! El servicio fue excelente y la comida estuvo deliciosa. Recomiendo especialmente la pasta con mariscos, ¡espectacular!' 
+    },
+    { rating: "4", 
+      name: "Carlos Perez", 
+      message: 'El lugar tiene un ambiente acogedor y la comida estuvo muy buena, aunque el tiempo de espera fue un poco largo. Ideal para una cena tranquila.' 
+    },
+    { rating: "3", 
+      name: "Maria Lopez", 
+      message: 'La comida estaba bien, pero esperaba un poco más de sabor en algunos platos. El servicio fue amable, aunque un poco lento en algunos momentos.' 
+    },
+    { rating: "5", 
+      name: "Sofia Fernandez", 
+      message: 'Celebramos un evento privado y todo salió perfecto. El personal estuvo muy atento. El banquete fue exquisito, nuestros invitados quedaron encantados.' 
+    },
+  ];
+
   constructor(private toastr: ToastrService) {}
 
   newContact(form: NgForm) {
@@ -28,15 +47,18 @@ export class ContactComponent {
     }
   }
 
-  newReview(reviewForm: NgForm) {
-    if (reviewForm.valid) {
-      const review = {
-        rating: reviewForm.value.rating,
-        message: reviewForm.value.reviewMessage
+  newReview(form: NgForm) {
+    if (form.valid) {
+      const newReview = {
+        rating: form.value.rating,
+        name: form.value.name,
+        message: form.value.reviewMessage
       };
-  
+
+      this.reviews.push(newReview)
+
       this.toastr.success('¡Gracias por tu valoración!');
-      reviewForm.resetForm();
+      form.resetForm();
 
     } else {
       console.log("Formulario de valoración inválido");
