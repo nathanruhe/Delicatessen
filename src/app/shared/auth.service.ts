6b4825ad5ee2ee308f7fd8f1800a9cb79ac8ac8a
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-21 15:04:50 +0100

    auth service fix

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -7,15 +7,23 @@ export class AuthService {
 
   private isAuthenticated: boolean = false;
 
+  constructor() {
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    this.isAuthenticated = storedAuth === 'true';
+  }
+
   login() {
     this.isAuthenticated = true;
+    localStorage.setItem('isAuthenticated', 'true');
   }
 
   logout() {
     this.isAuthenticated = false;
+    localStorage.setItem('isAuthenticated', 'false');
   }
 
   isLoggedIn(): boolean {
-    return this.isAuthenticated;
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    return storedAuth === 'true';
   }
 }
